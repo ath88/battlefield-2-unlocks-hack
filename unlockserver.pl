c174b0fe70ec2dca1629bdfe54d8385517d235e4
Asbjørn Thegler <asbjoern@gmail.com>
2014-02-26 18:55:23 +0100

    beautified with Perl::Tidy

--- unlockserver.pl
+++ unlockserver.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/env perl
+
+use strict;
+use warnings;
+
 use Mojolicious::Lite;
-use lib '.';
-use English;
 
 # remember to add the following to your hosts-file
 # 127.0.0.1 bf2web.gamespy.com
@@ -10,7 +12,7 @@ app->secret('secretbf2hackedserverxor');
 
 get '/ASP/getunlocksinfo' => sub {
   my $self = shift;
-  my $pid = $self->param('pid');
+  my $pid  = $self->param('pid');
   my $body;
 
   $self->app->log->info("Request made for pid: [$pid]");
@@ -19,39 +21,38 @@ get '/ASP/getunlocksinfo' => sub {
   $self->res->headers->content_type('text/html; charset=utf-8');
   $self->res->body($body);
   $self->res->fix_headers;
-  $self->res->headers->header('Cache-Control' => 'private');
-  $self->res->headers->header('Server' => 'Microsoft-IIS/6.0');
-  $self->res->headers->header('X-AspNet-Version' => '2.0.50727');
-  $self->res->headers->header('X-Powered-By' => 'ASP.NET');
-  $self->res->headers->header('cluster-server' => 'gstpsprdweb08');
-  $self->res->headers->header('p3p' => "CP='NOI ADMa OUR STP'");
+  $self->res->headers->header( 'Cache-Control'    => 'private' );
+  $self->res->headers->header( 'Server'           => 'Microsoft-IIS/6.0' );
+  $self->res->headers->header( 'X-AspNet-Version' => '2.0.50727' );
+  $self->res->headers->header( 'X-Powered-By'     => 'ASP.NET' );
+  $self->res->headers->header( 'cluster-server'   => 'gstpsprdweb08' );
+  $self->res->headers->header( 'p3p'              => q(CP='NOI ADMa OUR STP') );
 
-  $self->write( $body );
+  $self->write($body);
   $self->finish();
 };
 
 # next route is only necessary if you dont run a dedicated server
 get '/*uniqueblah' => sub {
-  my $self = shift;
-  my $all = $self->param('uniqueblah');
+  my $self   = shift;
+  my $all    = $self->param('uniqueblah');
   my @params = $self->param;
   my $params;
   foreach my $param (@params) {
     next if $param eq 'uniqueblah';
-    $params .= "$param=" . $self->param($param) . "&";
+    $params .= "$param=" . $self->param($param) . '&';
   }
   my $path = "69.10.24.134/$all?$params";
   $self->app->log->debug("Irrelevant URL. Forwarding to $all.");
-  my $response = $self->ua->get($path => { Host => 'bf2web.gamespy.com' });
-  $self->render(text => $response->res->body);
+  my $response = $self->ua->get( $path => { Host => 'bf2web.gamespy.com' } );
+  $self->render( text => $response->res->body );
 };
 
 app->start;
 
-
 sub stringify {
-  my $pid = shift;
-  my $epoch = time();
+  my $pid   = shift;
+  my $epoch = time;
 
   my $string = "O\n";
   $string .= "H\tpid\tnick\tasof\n";
@@ -75,18 +76,17 @@ sub stringify {
   $string .= "D\t555\ts\n";
 
   my $length = chars($string);
-  
-  $string .= "\$\t$length\t\$\n"; 
+
+  $string .= "\$\t$length\t\$\n";
   return $string;
 }
 
 sub chars {
   my ($string) = @_;
- 
+
   $string =~ s/\t//g;
   $string =~ s/\n//g;
-  return length($string);
+  return length $string;
 }
-
 
 1;
